147f8bad8062c1cc237a91f21db7b3df3c6f9725
Natnael Haile <natnaelhaile@Natnaels-MacBook-Pro.local>
2021-06-24 00:25:46 -0400

    created a timer

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
+function time() {
+  $("#today-time").empty(); 
+  let currentTime = moment().format("hh:mm:ss");
+  $("#today-time").append(currentTime);
+}
+setInterval(time, 1000)
+
+
+let currentDay = moment().format("MMM Do, YYYY");
+$("#today-date").append(currentDay);
+
 let now = new Date().getHours();
 
 function assignColor() {
-  var startTime = 9;
-  var endTime = 17;
+  let startTime = 9;
+  let endTime = 17;
   for (x = startTime; x <= endTime; x++) {
     var styleClass;
     if (x < now) {
@@ -15,16 +26,16 @@ function assignColor() {
     $("#note-" + x).addClass(styleClass);
     document.getElementById(x.toString()).addEventListener("click", updateText);
 
-    var textBoxValue = localStorage.getItem("text-" +x);
+    let textBoxValue = localStorage.getItem("text-" + x);
     document.querySelector("#note-" + x).value = textBoxValue;
   }
 }
 assignColor();
 
 function updateText(e) {
-    var id = e.srcElement.id;
-    localStorage.setItem("text-"+id , document.querySelector("#note-"+id).value);
+  let id = e.srcElement.id;
+  localStorage.setItem(
+    "text-" + id,
+    document.querySelector("#note-" + id).value
+  );
 }
-
-
-
